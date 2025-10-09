0666dea0ac469a9e618f7bec80fd4fd209b496df
Rigonkmalk <rigonkmalk@gmail.com>
2025-10-09 17:49:04 +0200

    fix: EOF

--- extract_result.sh
+++ extract_result.sh
@@ -11,7 +11,8 @@ suppress_old_file() {
         for i in *.sql; do
             rm "$i"
         done
-    fi}
+    fi
+}
 
 extract_result() {
     extract=$(for i in $(mysql -Ne "select distinct TABLE_NAME from information_schema.partitions where TABLE_SCHEMA='centreon_storage' and (TABLE_NAME like 'mod_bi%' OR TABLE_NAME like 'data_bin') and PARTITION_NAME is NOT NULL;"); do echo $i && mysql -e "select from_unixtime(PARTITION_DESCRIPTION), PARTITION_DESCRIPTION, PARTITION_ORDINAL_POSITION, TABLE_ROWS from information_schema.partitions where table_schema = 'centreon_storage' and table_name = '$i' order by PARTITION_ORDINAL_POSITION desc;";done)
