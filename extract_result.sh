48c424cd28503214b5fe8fa323da153b59302e2f
Rigonkmalk <rigonkmalk@gmail.com>
2025-10-09 17:44:20 +0200

    feat(shell): process to delete old files

--- extract_result.sh
+++ extract_result.sh
@@ -1,9 +1,19 @@
 #!/bin/bash -e
 # Extract the result.txt file from Confluence Documentation related to the partitioned table's analysis.
 
+suppress_old_file() {
+    for i in *.txt; do
+        rm "$i"
+    done
+    for i in *.sql; do
+        rm "$i"
+    done
+}
+
 extract_result() {
     extract=$(for i in $(mysql -Ne "select distinct TABLE_NAME from information_schema.partitions where TABLE_SCHEMA='centreon_storage' and (TABLE_NAME like 'mod_bi%' OR TABLE_NAME like 'data_bin') and PARTITION_NAME is NOT NULL;"); do echo $i && mysql -e "select from_unixtime(PARTITION_DESCRIPTION), PARTITION_DESCRIPTION, PARTITION_ORDINAL_POSITION, TABLE_ROWS from information_schema.partitions where table_schema = 'centreon_storage' and table_name = '$i' order by PARTITION_ORDINAL_POSITION desc;";done)
     echo "$extract"
 }
 
+suppress_old_file
 extract_result
